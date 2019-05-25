e16939d9bd4381dfadfabaf9b626d479a132ffe8
hugo lazzari <hugo.luiz@outlook.com>
2019-05-25 00:17:38 -0300

    added rest of the features

--- jason/src/app/app.module.ts
+++ jason/src/app/app.module.ts
@@ -8,15 +8,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PeopleService } from './people.service';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+// tslint:disable-next-line: deprecation
+    HttpModule
+    ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide:
+      RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    },
+    PeopleService
   ],
   bootstrap: [AppComponent]
 })
--- jason/src/app/home/home.page.ts
+++ jason/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PeopleService } from '../people.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,59 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public ima = Array<any>();
+  public info: any;
+  public image: any;
+  public works: any;
+  public baio: any;
+  public name: any;
+  public bday: any;
+  public pob: any;
+  public bio: any;
+
+  constructor(
+    public route: ActivatedRoute,
+    public people: PeopleService
+  ) {
+    this.works = this.people.getToWork().subscribe(
+      data => {
+        const dataa = (data as any);
+        const amy = JSON.parse(dataa._body);
+        console.log(amy);
+        this.baio = amy.cast;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+
+    this.image = this.people.getTaiChi().subscribe(
+      data => {
+        const tada = (data as any);
+        const yam = JSON.parse(tada._body);
+        console.log(yam);
+        this.ima = yam.profiles;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+
+    this.info = this.people.getJohnWick().subscribe(
+      data => {
+        const atad = (data as any);
+        const may = JSON.parse(atad._body);
+        console.log(data as any);
+        console.log(may);
+        this.name = may.name;
+        this.bday = may.birthday;
+        this.pob = may.place_of_birth;
+        this.bio = may.biography;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 
 }
