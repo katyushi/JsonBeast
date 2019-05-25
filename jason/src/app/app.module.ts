66d516539f1e0958b7d282a64d46bc95bc5c722f
hugo lazzari <hugo.luiz@outlook.com>
2019-05-25 00:24:50 -0300

    reformated code with webstorm

--- jason/src/app/app.module.ts
+++ jason/src/app/app.module.ts
@@ -1,35 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { PeopleService } from './people.service';
-import { HttpModule } from '@angular/http';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {PeopleService} from './people.service';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
 // tslint:disable-next-line: deprecation
-    HttpModule
+        HttpModule
     ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide:
-      RouteReuseStrategy,
-      useClass: IonicRouteStrategy
-    },
-    PeopleService
-  ],
-  bootstrap: [AppComponent]
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {
+            provide:
+            RouteReuseStrategy,
+            useClass: IonicRouteStrategy
+        },
+        PeopleService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- jason/src/app/people.service.ts
+++ jason/src/app/people.service.ts
@@ -1,24 +1,28 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PeopleService {
 
-  private basicApiPath = 'https://api.themoviedb.org/3/person';
+    private basicApiPath = 'https://api.themoviedb.org/3/person';
+
 // tslint:disable-next-line: deprecation
-  constructor(public http: Http) {
-    console.log('hello beast');
-  }
-  getToWork() {
-    return this.http.get(this.basicApiPath + '/6384/combined_credits?api_key=634232f1957195aa41bb453205010974&language=pt-BR');
-  }
-    getTaiChi() {
-      return this.http.get(this.basicApiPath + '/6384/images?api_key=634232f1957195aa41bb453205010974');
+    constructor(public http: Http) {
+        console.log('hello beast');
     }
-  getJohnWick() {
-    return this.http.get(this.basicApiPath + '/6384?api_key=634232f1957195aa41bb453205010974&language=pt-BR');
-  }
+
+    getToWork() {
+        return this.http.get(this.basicApiPath + '/6384/combined_credits?api_key=634232f1957195aa41bb453205010974&language=pt-BR');
+    }
+
+    getTaiChi() {
+        return this.http.get(this.basicApiPath + '/6384/images?api_key=634232f1957195aa41bb453205010974');
+    }
+
+    getJohnWick() {
+        return this.http.get(this.basicApiPath + '/6384?api_key=634232f1957195aa41bb453205010974&language=pt-BR');
+    }
 }
